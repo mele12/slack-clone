1106b2dffee54af1ab69983f92c5e91ce52fcc2a
melaku-k <kassamelaku2129@outlook.com>
2021-03-29 02:19:05 -0500

    slack

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+        <h1>test</h1>
+        </HeaderContainer>
+    )
+}
+
+export default Header
+
+const HeaderContainer = styled.div`
+background-color: red;
+`;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhZy5JNHHN74vyYr_hSTzSmV3lXagzrFI",
+    authDomain: "slack-clone-80416.firebaseapp.com",
+    projectId: "slack-clone-80416",
+    storageBucket: "slack-clone-80416.appspot.com",
+    messagingSenderId: "26151976871",
+    appId: "1:26151976871:web:aeae2f016c3f46be77c082",
+    measurementId: "G-47J657TJZ5"
+  };
